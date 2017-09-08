c374a5e6b481554ddd99d9c5ebb5a938bb5b49f8
yunfeiroy666 <yunfeiroy@gmail.com>
2017-09-08 20:57:38 +0800

    add register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
 
 from . import auth
-from flask_login import login_required, login_user, logout_user
+from flask_login import login_required, login_user, logout_user, current_user
 from ..models import User
 from .forms import LoginForm, RegistrationForm
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -35,4 +36,28 @@ def logout():
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    pass
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
+
+
+# @auth.route('/confirm/<token>')
+# @login_required
+# def confirm(token):
+#     if current_user.confirmed:
+#         return redirect(url_for('main.index'))
+#     if current_user.confirm(token):
+#         flash('You have confirmed your account. Thanks')
+#     else:
+#         flash('The confirmation link is invalid or has expired')
+#     return redirect(url_for('main.index'))
+#
+#
+# @auth.before_app_request
+# def before_request():
+#     if current_user.is_authenticated() and not current_user.confirmed and request.endpoint[:5] != 'auth.':
